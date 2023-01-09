247d0113c5a202693c9a423a74f01e81b839e25d
Jesus <jrgaleasantiago98@gmail.com>
2023-01-09 14:21:49 +0100

    xd

--- Spring/bibliotecapp/src/main/java/com/Jesusgalea/bibliotecapp/BibliotecappApplication.java
+++ Spring/bibliotecapp/src/main/java/com/Jesusgalea/bibliotecapp/BibliotecappApplication.java
@@ -1,13 +1,37 @@
 package com.Jesusgalea.bibliotecapp;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.Jesusgalea.bibliotecapp.model.Categoria;
+import com.Jesusgalea.bibliotecapp.repository.CategoriasRepository;
+
 @SpringBootApplication
-public class BibliotecappApplication {
+public class BibliotecappApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriasRepository categoriasRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecappApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		buscarTodosJpa();
+		
+	}
+	
+	private List<Categoria> buscarTodosJpa(){
+		List<Categoria> categorias = categoriasRepo.findAll();
+		for (Categoria c : categorias) {
+			System.out.println(c.getId() + " " + c.getNombre());
+		}
+		return categorias;
+		
 	}
 
 }
---
+++ Spring/bibliotecapp/src/main/java/com/Jesusgalea/bibliotecapp/repository/CategoriasRepository.java
@@ -0,0 +1,9 @@
+package com.Jesusgalea.bibliotecapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Jesusgalea.bibliotecapp.model.Categoria;
+
+public interface CategoriasRepository extends JpaRepository<Categoria, Integer> {
+
+}
